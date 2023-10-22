5747ab3fa763039e3729d3b089593fcacf831697
EnzoPB <neloxxfx@gmail.com>
2023-10-22 19:52:56 +0200

    improved icon handling & management

--- portfolio/components/svg/svg.py
+++ portfolio/components/svg/svg.py
@@ -1,14 +1,23 @@
+from django.conf import settings
+from django.db.models.fields.files import FieldFile
 from django_components import component
 from django.contrib.staticfiles import finders
-import sys
+import os
 @component.register('svg')
 class Svg(component.Component):
     template_name = 'svg/svg.html'
 
     def get_context_data(self, src):
-        src = finders.find(src)
-        with open(src, 'r') as f:
-            svg_content = f.read()
+        if isinstance(src, FieldFile):
+            src = src.path
+        else:
+            src = finders.find(src)
+
+        if src is not None:
+            with open(src, 'r') as f:
+                svg_content = f.read()
+        else:
+            svg_content = ''
 
         return {
             'svg_content': svg_content
--- portfolio/models.py
+++ portfolio/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 
+from uuid import uuid4
+
+def random_filename(instance, filename):
+    ext = filename.split('.')[-1]
+    return f'{uuid4()}.{ext}'
+
 
 class ProjectSkill(models.Model):
     name = models.CharField(max_length=50)
-    icon = models.CharField(max_length=20)
+    icon = models.FileField(upload_to=random_filename)
 
     def __str__(self):
         return self.name
@@ -12,12 +18,11 @@ class ProjectSkill(models.Model):
         verbose_name = 'Skill'
         verbose_name_plural = 'Skills'
 
-
 class Project(models.Model):
     title = models.CharField(max_length=50)
     short_description = models.CharField('short description', max_length=200)
     description = models.TextField()
-    image = models.ImageField(upload_to='uploads/')
+    image = models.ImageField(upload_to=random_filename)
     skills = models.ManyToManyField(ProjectSkill)
 
     def __str__(self):
@@ -27,7 +32,7 @@ class Project(models.Model):
 class ProjectLink(models.Model):
     label = models.CharField(max_length=50)
     url = models.URLField(max_length=200)
-    icon = models.CharField(max_length=20)
+    icon = models.FileField(upload_to=random_filename)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
 
     def __str__(self):
