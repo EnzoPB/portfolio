c4ec872222a1efa08c74121812b14d3e7cfe3c2b
Enzo <enzo.puiroux@mail.com>
2024-01-09 22:20:06 +0100

    changed WEBSITE_HOSTNAME to HOSTNAME

--- enzopbme/settings.py
+++ enzopbme/settings.py
@@ -25,8 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
 
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
-CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
+ALLOWED_HOSTS = [os.environ['HOSTNAME']] if 'HOSTNAME' in os.environ else []
+CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['HOSTNAME']] if 'HOSTNAME' in os.environ else []
 DEBUG = True
 
 
